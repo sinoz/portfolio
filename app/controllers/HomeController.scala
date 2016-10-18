eba14bf3398333e729d263a56ce2d47008b71c30
Sino <ilyas.alfan.b@gmail.com>
2016-10-19 00:02:10 +0200

    Receives POST requests from the form and transforms the data into a ContactRequest for further validation

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,17 +1,44 @@
 package controllers
 
-import javax.inject._
-import play.api._
+import com.google.inject.{Inject, Singleton}
+import play.api.data.Forms._
+import play.api.data._
 import play.api.mvc._
 
 /** The controller of the home page. */
 @Singleton
 class HomeController @Inject() extends Controller {
+  /** Maps filled in user information from the contact form to a ContactRequest. */
+  private implicit val contactForm = Form(
+    mapping(
+      "name" -> nonEmptyText,
+      "surname" -> nonEmptyText,
+      "email" -> email,
+      "phone" -> text,
+      "message" -> text
+    )
+    (ContactRequest.apply)(ContactRequest.unapply)
+  )
+
+  /** Returns the index page synchronously. */
   def index = Action {
     Ok(views.html.index())
   }
 
-  def contactPost = Action {
-    Ok(views.html.index())
+  /** Synchronously obtains the contact request and handles it. */
+  def contactPost = Action { implicit request =>
+    contactForm.bindFromRequest().fold(
+      formWithErrors => {
+        BadRequest(views.html.index())
+      },
+
+      contact => {
+        // TODO
+
+        Ok(views.html.index())
+      }
+    )
   }
 }
+
+case class ContactRequest(name: String, surname: String, email: String, phone: String, message: String)
